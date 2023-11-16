54fca3d25b3bbf7734b7e93840fd341936515790
tom <tom_deprez@hotmail.fr>
2023-11-16 16:56:56 +0100

    ajouter une function

--- exo1.php
+++ exo1.php
@@ -1,33 +1,33 @@
 <?php
-
-$_POST[""];
-$_GET[""];
-
-$num1 = 0;
+$num1 = 5;
 $num2 = 10;
 
-$operation = "dzqgjdfzj";
-$result = 0;
-
-$testnum = "10";
+$operation = "+";
+calcul(10, 5, "+");
 
 
-if ($num2 === $testnum) {
-    $result = "OK";
-}
-
-$testBool = (($operation == "+" && $num1 > 10) || ($num2 > 5 && $num2 < 20));
-
-if (($operation == "+" && $num1 > 10) || $num2 < 20) {
-    $result = $num1 + $num2;
-}
-if ($operation == "-") {
-    $result = $num1 - $num2;
-}
-if ($operation == "/") {
-    $result = $num1 / $num2;
-}
-if ($operation == "*") {
-    $result = $num1 * $num2;
-}
 echo $result;
+
+
+function calcul($num1, $num2, $operation)
+{
+
+    $result = 0;
+
+    $testnum = "10";
+
+
+
+    if ($operation == "+") {
+        $result = $num1 + $num2;
+    }
+    if ($operation == "-") {
+        $result = $num1 - $num2;
+    }
+    if ($operation == "/") {
+        $result = $num1 / $num2;
+    }
+    if ($operation == "*") {
+        $result = $num1 * $num2;
+    }
+}
