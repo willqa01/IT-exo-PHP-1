043adb99c0f24fcd8feaca07b41d8949e7014d04
tom <tom_deprez@hotmail.fr>
2023-11-20 13:43:17 +0100

    modification de la class

--- Departement.php
+++ Departement.php
@@ -1,29 +1,2 @@
 
 <?php
-class Departement {
-    private $nom;
-    private $employes = [];
-
-    public function __construct($nom) {
-        $this->nom = $nom;
-    }
-
-    public function ajouterEmploye($employe) {
-        $this->employes[] = $employe;
-    }
-
-    public function afficherEmployes() {
-        echo "Employés du département " . $this->nom . ":\n";
-        foreach ($this->employes as $employe) {
-            $employe->afficherInfos();
-        }
-    }
-
-    public function getNom(){
-        return $this->nom;
-    }
-
-    public function setNom($nom){
-        $this->nom = $nom;
-    }
-}
--- Employe.php
+++ Employe.php
@@ -15,7 +15,33 @@ class Employe {
         echo "Nom: " . $this->nom . ", Age: " . $this->age . ", Département: " . $this->departement . "\n";
     }
 }
+class Departement {
+    private $nom;
+    private $employes = [];
 
+    public function __construct($nom) {
+        $this->nom = $nom;
+    }
+
+    public function ajouterEmploye($employe) {
+        $this->employes[] = $employe;
+    }
+
+    public function afficherEmployes() {
+        echo "Employés du département " . $this->nom . ":\n";
+        foreach ($this->employes as $employe) {
+            $employe->afficherInfos();
+        }
+    }
+
+    public function getNom(){
+        return $this->nom;
+    }
+
+    public function setNom($nom){
+        $this->nom = $nom;
+    }
+}
 
 // Utilisation des classes
 $departementIT = new Departement("Informatique");
