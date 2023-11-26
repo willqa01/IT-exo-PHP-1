9f9caa56b715ffed9de10593a4822a800622f869
TomTom <tom_deprez@hotmail.fr>
2023-11-26 18:59:25 +0100

    Ajout de 4 bugs pour tests sur la nouvelle page
    - Intégration de 2 bugs détectables via tests automatisés avec Selenium.
    - Ajout de 2 bugs fonctionnels pour évaluation manuelle.
    
    Ces bugs sont destinés à évaluer les capacités de détection des anomalies par les tests automatisés et manuels sur la nouvelle page de l'application.

--- projet/form.php
+++ projet/form.php
@@ -14,13 +14,13 @@
     <!-- Bootstrap core CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <!-- Custom styles for this template -->
-    <link href="form-validation.css" rel="stylesheet">
+    <link href="css/form.css" rel="stylesheet">
 </head>
 
 <body class="bg-light">
-<?php
-  include 'navbar.php';
-  ?>
+    <?php
+    include 'navbar.php';
+    ?>
     <div class="container">
         <div class="py-5 text-center">
             <img class="d-block mx-auto mb-4" src="https://getbootstrap.com/docs/4.0/assets/brand/bootstrap-solid.svg" alt="" width="72" height="72">
@@ -34,38 +34,11 @@
                     <span class="text-muted">Your cart</span>
                     <span class="badge badge-secondary badge-pill">3</span>
                 </h4>
-                <ul class="list-group mb-3">
-                    <li class="list-group-item d-flex justify-content-between lh-condensed">
-                        <div>
-                            <h6 class="my-0">Product name</h6>
-                            <small class="text-muted">Brief description</small>
-                        </div>
-                        <span class="text-muted">$12</span>
-                    </li>
-                    <li class="list-group-item d-flex justify-content-between lh-condensed">
-                        <div>
-                            <h6 class="my-0">Second product</h6>
-                            <small class="text-muted">Brief description</small>
-                        </div>
-                        <span class="text-muted">$8</span>
-                    </li>
-                    <li class="list-group-item d-flex justify-content-between lh-condensed">
-                        <div>
-                            <h6 class="my-0">Third item</h6>
-                            <small class="text-muted">Brief description</small>
-                        </div>
-                        <span class="text-muted">$5</span>
-                    </li>
-                    <li class="list-group-item d-flex justify-content-between bg-light">
-                        <div class="text-success">
-                            <h6 class="my-0">Promo code</h6>
-                            <small>EXAMPLECODE</small>
-                        </div>
-                        <span class="text-success">-$5</span>
-                    </li>
+                <ul class="list-group mb-3" id="cartList">
+
                     <li class="list-group-item d-flex justify-content-between">
                         <span>Total (USD)</span>
-                        <strong>$20</strong>
+                        <strong id="total">$0</strong>
                     </li>
                 </ul>
 
@@ -73,14 +46,34 @@
                     <div class="input-group">
                         <input type="text" class="form-control" placeholder="Promo code">
                         <div class="input-group-append">
-                            <button type="submit" class="btn btn-secondary">Redeem</button>
+                            <button type="submit" class="btn btn-secondary" style="height: 100%;">Redeem</button>
                         </div>
                     </div>
                 </form>
+                <!-- Modale d'erreur -->
+                <div class="modal fade" id="errorModal" tabindex="-1" role="dialog" aria-labelledby="errorModalLabel" aria-hidden="true">
+                    <div class="modal-dialog" role="document">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="errorModalLabel">Erreur Système</h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div class="modal-body">
+                                Une erreur s'est produite. Veuillez réessayer plus tard.
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Fermer</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
             </div>
             <div class="col-md-8 order-md-1">
                 <h4 class="mb-3">Billing address</h4>
-                <form class="needs-validation" novalidate="">
+                <form class="needs-validation" novalidate="" id="checkoutFormValid">
                     <div class="row">
                         <div class="col-md-6 mb-3">
                             <label for="firstName">First name</label>
@@ -102,7 +95,7 @@
                         <label for="username">Username</label>
                         <div class="input-group">
                             <div class="input-group-prepend">
-                                <span class="input-group-text">@</span>
+                                <span class="input-group-text" style="height: 100%;">@</span>
                             </div>
                             <input type="text" class="form-control" id="username" placeholder="Username" required="">
                             <div class="invalid-feedback" style="width: 100%;">
@@ -119,7 +112,7 @@
                         </div>
                     </div>
 
-                    <div class="mb-3">
+                    <div class="mb-3" id="bug1">
                         <label for="address">Address</label>
                         <input type="text" class="form-control" id="address" placeholder="1234 Main St" required="">
                         <div class="invalid-feedback">
@@ -162,6 +155,13 @@
                         </div>
                     </div>
                     <hr class="mb-4">
+                    <div class="mb-3" id="detailsField" style="display: none;">
+                        <label for="details">Details</label>
+                        <input type="text" class="form-control" id="details" placeholder="Enter details" required>
+                        <div class="invalid-feedback">
+                            Details are required.
+                        </div>
+                    </div>
                     <div class="custom-control custom-checkbox">
                         <input type="checkbox" class="custom-control-input" id="same-address">
                         <label class="custom-control-label" for="same-address">Shipping address is the same as my billing address</label>
@@ -222,7 +222,7 @@
                         </div>
                     </div>
                     <hr class="mb-4">
-                    <button class="btn btn-primary btn-lg btn-block" type="submit">Continue to checkout</button>
+                    <button class="btn btn-primary btn-lg btn-block" type="submit" id="checkoutForm" onclick="onFormSubmit(event)">Continue to checkout</button>
                 </form>
             </div>
         </div>
@@ -240,13 +240,12 @@
     <!-- Bootstrap core JavaScript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script>
-        window.jQuery || document.write('<script src="../../assets/js/vendor/jquery-slim.min.js"><\/script>')
-    </script>
-    <script src="../../assets/js/vendor/popper.min.js"></script>
-    <script src="../../dist/js/bootstrap.min.js"></script>
-    <script src="../../assets/js/vendor/holder.min.js"></script>
+    <!-- Inclusion de jQuery -->
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+
+    <!-- Inclusion de Bootstrap JS -->
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
     <script>
         // Example starter JavaScript for disabling form submissions if there are invalid fields
         (function() {
@@ -268,6 +267,174 @@
                 });
             }, false);
         })();
+        // Fonction pour afficher la modale d'erreur
+        function showErrorModal() {
+            $('#errorModal').modal('show');
+        }
+        document.querySelector('.card.p-2').addEventListener('submit', function(event) {
+            event.preventDefault(); // Empêche la soumission standard du formulaire
+            showErrorModal(); // Affiche la modale d'erreur
+        });
+
+
+        // Fonction pour mettre à jour le total du panier
+        function updateTotal(newPrice) {
+            var totalItem = document.getElementById('total');
+            var currentTotal = totalItem.textContent.replace('$', '');
+            var newTotal = parseFloat(currentTotal) + newPrice;
+            totalItem.textContent = '$' + newTotal.toFixed(2);
+        }
+
+        // Fonction pour ajouter un article au panier
+        // Fonction pour ajouter un article au panier
+        // Fonction pour ajouter un article au panier
+        function addToCart(productName, productDescription, productPrice) {
+            var cartList = document.getElementById('cartList');
+            var newItem = document.createElement('li');
+            newItem.className = 'list-group-item d-flex justify-content-between lh-condensed';
+            newItem.innerHTML = `
+        <div>
+            <h6 class="my-0">${productName}</h6>
+            <small class="text-muted">${productDescription}</small>
+        </div>
+        <span class="text-muted">$${productPrice.toFixed(2)}</span>
+    `;
+
+            // Mise à jour du total et affichage de la TVA
+            updateTotal(productPrice);
+            displayTax();
+
+            // Trouver l'index de la ligne de la TVA
+            var taxIndex;
+            for (var i = 0; i < cartList.children.length; i++) {
+                if (cartList.children[i].id === 'taxItem') {
+                    taxIndex = i;
+                    break;
+                }
+            }
+
+            // Insérer le nouvel article après la ligne de la TVA
+            if (taxIndex !== undefined) {
+                cartList.insertBefore(newItem, cartList.children[taxIndex + 1]);
+            } else {
+                // S'il n'y a pas de ligne de TVA, ajouter à la fin
+                cartList.appendChild(newItem);
+            }
+        }
+
+
+        // Fonction pour calculer et afficher la TVA
+        function displayTax() {
+            var totalItem = document.getElementById('total');
+            var currentTotal = parseFloat(totalItem.textContent.replace('$', ''));
+            var taxRate = 0.20; // Taux de TVA de 20%
+            var taxAmount = currentTotal * taxRate / 10;
+
+            var cartList = document.getElementById('cartList');
+            var taxItem = document.getElementById('taxItem'); // Recherche d'un élément existant avec l'id 'taxItem'
+
+            if (taxItem) {
+                // Si la ligne de TVA existe, mettre à jour son contenu
+                taxItem.innerHTML = `
+            <div>
+                <h6 class="my-0">Tax (20%)</h6>
+            </div>
+            <span class="text-muted">$${taxAmount.toFixed(2)}</span>
+        `;
+            } else {
+                // Si elle n'existe pas, créer une nouvelle ligne et l'ajouter
+                taxItem = document.createElement('li');
+                taxItem.id = 'taxItem'; // Assurez-vous que cet ID est unique
+                taxItem.className = 'list-group-item d-flex justify-content-between lh-condensed';
+                taxItem.innerHTML = `
+            <div>
+                <h6 class="my-0">Tax (20%)</h6>
+            </div>
+            <span class="text-muted">$${taxAmount.toFixed(2)}</span>
+        `;
+                cartList.appendChild(taxItem);
+            }
+        }
+
+        // Fonction appelée lors de la soumission du formulaire
+        function onFormSubmit(event) {
+            var form = document.getElementById('checkoutFormValid'); // Assurez-vous que l'ID est correct
+
+            if (form.checkValidity() === false) {
+                event.preventDefault();
+                event.stopPropagation();
+                form.classList.add('was-validated'); // Ajoute la classe pour montrer les erreurs de validation
+            } else {
+                event.preventDefault(); // Empêche la soumission standard du formulaire
+
+                // Récupération des données du formulaire
+                var firstName = document.getElementById('firstName').value;
+                var lastName = document.getElementById('lastName').value;
+                var productName = firstName + ' ' + lastName; // Exemple de nom de produit
+                var productDescription = 'Custom order'; // Description personnalisée
+                var productPrice = Math.random() * 100; // Génère un prix aléatoire
+
+                // Ajout de l'article au panier
+                addToCart(productName, productDescription, productPrice);
+            }
+        }
+        // Fonction pour lire la valeur d'un cookie
+        function getCookie(name) {
+            var value = "; " + document.cookie;
+            var parts = value.split("; " + name + "=");
+            if (parts.length == 2) return parts.pop().split(";").shift();
+            return null;
+        }
+
+        // Fonction pour écrire un cookie
+        function setCookie(name, value, days) {
+            var expires = "";
+            if (days) {
+                var date = new Date();
+                date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+                expires = "; expires=" + date.toUTCString();
+            }
+            document.cookie = name + "=" + (value || "") + expires + "; path=/";
+        }
+
+        // Compter les rechargements de la page
+        function countPageReloads() {
+            var reloadCount = parseInt(getCookie("reloadCount")) || 0;
+            reloadCount++;
+            setCookie("reloadCount", reloadCount, 7);
+
+            // Gérer l'ID du champ 'address'
+            var addressField = document.getElementById("address");
+            if (addressField && reloadCount % 5 === 0) {
+                var newId = 'address' + reloadCount;
+                addressField.id = newId;
+                setCookie("addressId", newId, 7); // Stocker le nouvel ID dans un cookie
+            } else {
+                var storedId = getCookie("addressId");
+                if (storedId && addressField) {
+                    addressField.id = storedId;
+                }
+            }
+        }
+
+        // Appeler la fonction au chargement de la page
+        window.onload = countPageReloads;
+
+        document.getElementById('country').addEventListener('change', function() {
+            var selectedCountry = this.value;
+            var detailsField = document.getElementById('detailsField');
+
+            // Simuler un bug : le champ ne s'affiche pas toujours comme prévu
+            if (selectedCountry === 'United States') {
+                var shouldDisplay = Math.random() > 0.5; // 50% de chances d'afficher le champ
+                detailsField.style.display = shouldDisplay ? 'block' : 'none';
+            } else {
+                detailsField.style.display = 'none';
+            }
+        });
+
+
+        // Ajout de l'écouteur d'événement de soumission au formulaire
     </script>
 
 
